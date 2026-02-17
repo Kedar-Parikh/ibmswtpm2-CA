ef94f93db7ce875629e46b77f49d65e7c96b7560
Kedar Parikh <kedarparikh2016@gmail.com>
2026-02-17 23:49:47 +0530

    encrypt decrypt functions

--- workspace/TestTPM.py
+++ workspace/TestTPM.py
@@ -8,11 +8,25 @@ class TestTPM:
 
     def load_aes_key(self):
         subprocess.run([
-            "tpm2-load",
+            "tpm2_load",
             "-C", "primary.ctx",
             "-u", "aes.pub",
             "-r", "aes.priv",
             "-c", "aes.ctx"
         ])
 
-    
+    def encrypt(self, input, output):
+        subprocess.run([
+            "tpm2_encryptdecrypt",
+            "-c", "aes.ctx",
+            "-o", output,
+            input
+        ])
+
+    def decrypt(self, input, output):
+        subprocess.run([
+            "tpm2_encryptdecrypt", "-d"
+            "-c", "aes.ctx",
+            "-o", output,
+            input
+        ])
