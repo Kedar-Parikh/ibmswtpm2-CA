aefff323b46bf99d88a46d176aa2b4aad39616f3
Kedar Parikh <kedarparikh2016@gmail.com>
2026-04-03 15:58:49 +0530

    fixed minor bugs

--- workspace/TestTPM.py
+++ workspace/TestTPM.py
@@ -1,57 +1,37 @@
 import subprocess
-import time
-import os
+
 
 class TestTPM:
+    def start(self):
+        subprocess.run(["tpm2_startup", "-c"])
+
     def flushtpm(self):
         subprocess.run(["tpm2_flushcontext", "-t"])
-    
+
     def createprimary(self):
         subprocess.run([
-            "tpm2_createprimary", "-C", "o"
-            "-g", "sha256",
-            "-G", "rsa",
-            "-c", "primary.ctx"
+            "tpm2_createprimary", "-C", "o",
+            "-g", "sha256", "-G", "rsa", "-c", "primary.ctx"
         ])
 
     def createaeskeys(self):
         subprocess.run([
-            "tpm2_create", "-C", "primary.ctx"
-            "-G", "aes256",
-            "-u", "aes.pub",
-            "-r", "aes.priv",
-            "-c", "aes.ctx"
+            "tpm2_create", "-C", "primary.ctx",
+            "-G", "aes256cfb", "-u", "aes.pub", "-r", "aes.priv"
         ])
 
     def load_aes_key(self):
         subprocess.run([
-            "tpm2_load",
-            "-C", "primary.ctx",
-            "-u", "aes.pub",
-            "-r", "aes.priv",
-            "-c", "aes.ctx"
+            "tpm2_load", "-C", "primary.ctx",
+            "-u", "aes.pub", "-r", "aes.priv", "-c", "aes.ctx"
         ])
 
     def encrypt(self, input, output):
         subprocess.run([
-            "tpm2_encryptdecrypt",
-            "-c", "aes.ctx",
-            "-o", output,
-            input
+            "tpm2_encryptdecrypt", "-c", "aes.ctx", "-o", output, input
         ])
 
     def decrypt(self, input, output):
         subprocess.run([
-            "tpm2_encryptdecrypt", "-d"
-            "-c", "aes.ctx",
-            "-o", output,
-            input
+            "tpm2_encryptdecrypt", "-d", "-c", "aes.ctx", "-o", output, input
         ])
-
-    def start(self):
-        subprocess.run([
-            "tpm2_startup", "-c"
-            ])
-        
-
-# Tasks: create pilot script, create tester object, store data and plot dashboard to assess capabilities
