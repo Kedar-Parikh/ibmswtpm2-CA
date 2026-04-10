ff087209970767d9614218b1d75359f288c0d88e
Kedar Parikh <kedarparikh2016@gmail.com>
2026-04-10 11:49:44 +0530

    Added metrics measurement for aes encryption

--- workspace/main.py
+++ workspace/main.py
@@ -1,7 +1,11 @@
 import TestTPM
-import os, time
+import os, time, json
+import argparse
 
-filename = "plain.txt"
+parser = argparse.ArgumentParser(description="Script that supports TPM CA Research")
+parser.add_argument("plain_filename", help="Name of plaintext file")
+args = parser.parse_args()
+filename = args.plain_filename
 encryptedfilename = "cipher.bin"
 decryptedfilename = "decrypted.txt"
 
@@ -13,9 +17,37 @@ def return_function_time_taken(func, *args, **kwargs):
     return result
 
 
+def save_metrics_simple(filesize, enc_time, cipher_filesize, dec_time, enc_throughput, dec_throughput, filename="results.json"):
+    entry = {
+        "plain_filesize (bytes)": filesize,
+        "enc_time(s)": enc_time,
+        "cipher_filesize(bytes)": cipher_filesize,
+        "dec_time(s)": dec_time,
+        "enc_throughput(bytes/s)": enc_throughput,
+        "dec_throughput(bytes/s)": dec_throughput
+    }
+
+    data = []
+
+    
+    if os.path.exists(filename):
+        with open(filename, "r") as f:
+            try:
+                data = json.load(f)
+            except:
+                data = []
+
+    
+    data.append(entry)
+
+    
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+
 tpm = TestTPM.TestTPM()
 filesize = os.path.getsize(filename)
 result = tpm.initialisetpm()
+
 if(not result):
     flushtime = return_function_time_taken(tpm.flushtpm)
     createprimarytime = return_function_time_taken(tpm.createprimary)
@@ -27,10 +59,15 @@ if(not result):
     return_function_time_taken(tpm.flushtpm)
     return_function_time_taken(tpm.load_aes_key)
     aesdecrypttime = return_function_time_taken(tpm.decrypt, encryptedfilename, decryptedfilename)
+    cipher_filesize = os.path.getsize(encryptedfilename)
 
-    print(
-        f'Flush Time: {flushtime}\n Create Primary Time: {createprimarytime}\n Create AES Key Time: {createaestime}\n Load AES Time: {loadaestime}\n AES Encryption Time: {aesencrypttime}\n AES Decryption Time: {aesdecrypttime}\n'
-    )
+
+    encrypt_throughput = (filesize/aesencrypttime)
+    decrypt_throughput = (cipher_filesize/aesdecrypttime)
+
+    
+    save_metrics_simple(filesize, aesencrypttime, cipher_filesize, aesdecrypttime, encrypt_throughput, decrypt_throughput)
+    
 else:
     print("[!] Error Initialising TPM, please ensure server is running and connection variables are in order")
     exit(-1)
